5219cafcd501882e7ecd4a63097b89b5fec3ecc7
Amit Dharmandra Chaurasiya <Amit.Chaurasiya@asl.mum.sst>
2024-07-19 10:01:41 +0530

    Initial commit

---
+++ CurrencyCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCalculatorProject
+{
+    class CurrencyCalculator
+    {
+        private int _amount;
+        public int Amount
+        {
+            get { return _amount; }
+
+            set
+            {
+                if (value % 100 == 0 && value <= 50000)
+                {
+                    _amount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Amount Exceeds 50000");
+                }
+            }
+        }
+
+        public void CountCurrency(int amount)
+        {
+            int[] valueOfNotes = { 2000, 500, 200, 100 };
+            int temporaryVariable = amount;
+
+            foreach (int note in valueOfNotes)
+            {
+                Console.WriteLine($"{note} Note : {temporaryVariable / note}");
+                temporaryVariable = temporaryVariable % note;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CurrencyCalculatorProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the amout for denomination : ");
+        int amount = int.Parse(Console.ReadLine());
+        CurrencyCalculator calculateCurrency = new CurrencyCalculator();
+        calculateCurrency.Amount = amount;
+        calculateCurrency.CountCurrency(calculateCurrency.Amount);
+    }
+}
